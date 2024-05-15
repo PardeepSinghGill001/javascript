a3591b09c2004567180cefda45478fd587bfadc6
PardeepSinghGill001 <142096386+PardeepSinghGill001@users.noreply.github.com>
2024-05-15 07:51:43 +0000

    data_types

---
+++ 01_basics/01_data_types.js
@@ -0,0 +1,17 @@
+"use strict"; //all JS code will be treated as newer version by the JS engine
+alert("hello")//there is a different syntax to use alert in node, not like how it is in browser, where we write in console and get message after being processed by js engine in browser
+
+// number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+let state=null //state is not undefined it is null(empty)
+//undefined => represents empty value
+//symbol => uniqueness
+
+//object 
+
+console.log(typeof "Pardeep")
+console.log(typeof null)//type of null is object
+console.log(typeof undefined)//undefined
