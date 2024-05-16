7635722d397e0d2d7610541faa8f1c5aac9cfa4d
PardeepSinghGill001 <142096386+PardeepSinghGill001@users.noreply.github.com>
2024-05-16 16:28:47 +0000

    Conversions and Operators

---
+++ 01_basics/03_ConversionOperation.js
@@ -0,0 +1,45 @@
+// let score = 33
+  
+// console.log(typeof score)  //number
+// console.log(typeof(score)) //number
+
+// let score1="33"
+
+// console.log(typeof score1) //string
+// console.log(typeof(score1)) //string
+
+// let valueInNumber = Number(score1) //wrapper class
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+// let score2="33abc"
+// let valueInNumber1 = Number(score2)
+// console.log(valueInNumber1)
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1 //viceversa
+//false => 0 //viceversa
+//null => 0
+//undefined => NaN
+
+// *************Operations*************
+
+// let str1 = "hello"
+// let str2 = " world"
+// console.log(str1+str2);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+
+// write readable code 
+console.log(+true);
+console.log(+""); 
+
+
+
+
+
